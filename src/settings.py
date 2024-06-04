1820a8471402d27676f82cfa2a6b8e03341ae6f8
gandori <marcel.stiebing@web.de>
2024-06-04 20:40:27 +0200

    remove window_x_pos and window_y_pos from Settings class

--- src/settings.py
+++ src/settings.py
@@ -7,5 +7,3 @@ class Settings(BaseSettings):
     window_width: int = Field(default=1280, alias='WINDOW_WIDTH')
     window_height: int = Field(default=720, alias='WINDOW_HEIGHT')
     window_title: str = Field(default='Pygame', alias='WINDOW_TITLE')
-    window_x_pos: str = Field(default=0, alias='WINDOW_X_POS')
-    window_y_pos: str = Field(default=0, alias='WINDOW_Y_POS')
